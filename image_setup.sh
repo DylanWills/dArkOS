2eaf9807b054649e46b72e88c7baf7ea54e037c2
Christian Haitian <christianhaitian0@gmail.com>
2025-05-10 10:25:16 -0400

    New toolchains location needs su-su-sudo

--- image_setup.sh
+++ image_setup.sh
@@ -15,10 +15,10 @@ done
 
 # Verify the correct toolchain is available
 if [ ! -f "/opt/toolchains/gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu" ]; then
-  mkdir -p /opt/toolchains
+  sudo mkdir -p /opt/toolchains
   wget https://releases.linaro.org/components/toolchain/binaries/6.3-2017.05/aarch64-linux-gnu/gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu.tar.xz
   verify_action
-  tar Jxvf gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu.tar.xz -C /opt/toolchains/
+  sudo tar Jxvf gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu.tar.xz -C /opt/toolchains/
   rm gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu.tar.xz
 fi
 
